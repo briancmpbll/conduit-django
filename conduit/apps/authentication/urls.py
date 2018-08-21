cb95af8fdfea4b187f76be8641e699709f62706a
Brian Campbell <briancmpbll@gmail.com>
2018-08-21 15:02:53 -0700

    Add current user endpoints

--- conduit/apps/authentication/urls.py
+++ conduit/apps/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import RegistrationAPIView, LoginAPIView
+from .views import RegistrationAPIView, LoginAPIView, UserRetrievUpdateAPIView
 
 urlpatterns = [
+    url(r'^user/?$', UserRetrievUpdateAPIView.as_view()),
     url(r'^users/?$', RegistrationAPIView.as_view()),
-    url(r'^users/login/?$', LoginAPIView.as_view())
+    url(r'^users/login/?$', LoginAPIView.as_view()),
 ]
